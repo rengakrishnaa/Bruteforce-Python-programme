1e3b17af5b414535fca2147450b1dcdbe116b2bb
KRISHNAA R K <114340951+rengakrishnaa@users.noreply.github.com>
2023-10-23 22:24:00 +0530

    Add files via upload

---
+++ bruteforce.py
@@ -0,0 +1,28 @@
+from zipfile import ZipFile
+
+def extract(zip , password):
+    try:
+        zip.extractall(pwd=password)
+        return True
+    except:
+        return False
+
+
+def main():
+    print("[+] Beginning bruteforce ")
+    with ZipFile('enc.zip') as zf:
+        with open('rockyou.txt', 'rb') as f:
+
+            for i in f:
+                password = i.strip()
+                if extract(zip,password):
+                    print("Correct Password: %s" %password)
+                    exit(0)
+                
+                else:
+                    print("Incorrect password: %s" %password)
+
+        print("Password not found in list")
+
+if __name__ == "__main__":
+    main()
